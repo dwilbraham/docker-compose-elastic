e45df30895f84258d2956474fb00486e15765ce0
Dan Wilbraham <dwilbraham+git@gmail.com>
2016-03-27 02:54:55 +0100

    Add comments

--- bin/service_address.sh
+++ bin/service_address.sh
@@ -7,12 +7,15 @@ then
   exit 1
 fi
 
+# Check if docker-machine is installed
 if hash docker-machine 2>/dev/null
 then
   active=`docker-machine active 2>&1 || true`
 else
   active="No active host found"
 fi
+
+# Get port from docker-compose
 port=`docker-compose port $1 $2 | cut -d: -f2`
 
 if [ "_$active" = _"No active host found" ]
