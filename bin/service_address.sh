e7945b87f3c101980f85cc044463ccf1d842d163
Dan Wilbraham <dwilbraham+git@gmail.com>
2016-03-28 17:54:08 +0100

    Update service_address lookup to work with Docker Swarm clusters

--- bin/service_address.sh
+++ bin/service_address.sh
@@ -7,21 +7,31 @@ then
   exit 1
 fi
 
-# Check if docker-machine is installed
-if hash docker-machine 2>/dev/null
-then
-  active=`docker-machine active 2>&1 || true`
-else
-  active="No active host found"
-fi
-
 # Get port from docker-compose
+address=`docker-compose port $1 $2 | cut -d: -f1`
 port=`docker-compose port $1 $2 | cut -d: -f2`
 
-if [ "_$active" = _"No active host found" ]
+if [ $address != "0.0.0.0" ]
 then
-  echo localhost:$port
+  # Running with Docker Swarm address and port are correct
+  echo $address:$port
 else
-  echo `docker-machine ip $active`:$port
-fi
 
+  # Check if docker-machine is installed
+  if hash docker-machine 2>/dev/null
+  then
+    active=`docker-machine active 2>&1 || true`
+  else
+    active="No active host found"
+  fi
+
+  if [ "_$active" = _"No active host found" ]
+  then
+    #Running locally
+    echo localhost:$port
+  else
+    #Running with Docker Machine remotely
+    echo `docker-machine ip $active`:$port
+  fi
+
+fi
